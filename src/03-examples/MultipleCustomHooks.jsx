2c6b0a46937b94d21c918b5265bf14a4478305bc
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-23 22:46:07 -0700

    MultipleCustomHook Component

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,32 @@
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch";
+
+export const MultipleCustomHooks = () => {
+	const { counter, increment } = useCounter(1);
+	const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+	const { quote, author } = !!data && data[0];
+
+	if (hasError) {
+		return <div className="alert alert-danger text-center">Something ocurred contact the admin</div>;
+	}
+
+	return (
+		<>
+			<h1>Breaking Bad Quotes</h1>
+			<hr />
+
+			{isLoading ? (
+				<div className="alert alert-info text-center">Loading...</div>
+			) : (
+				<blockquote className="blockquote text-end">
+					<p className="mb-3">{quote}</p>
+					<footer className="blockquote-footer">{author}</footer>
+				</blockquote>
+			)}
+
+			<button onClick={() => increment(1)} className="btn btn-dark" disabled={isLoading}>
+				Next Quote
+			</button>
+		</>
+	);
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { HooksApp } from "./HooksApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<FormWithCustomHook />
-	</React.StrictMode>
+	// <React.StrictMode>
+	<MultipleCustomHooks />
+	// </React.StrictMode>
 );
