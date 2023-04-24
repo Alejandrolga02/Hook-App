2622902033972df8170f273c0aaea513eb63b072
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-23 19:18:41 -0700

    CounterApp changed state to object

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
 
 export const CounterApp = () => {
-	const [counter, setCounter] = useState(10);
+	const [{ counter1, counter2, counter3 }, setCounter] = useState({
+		counter1: 10,
+		counter2: 20,
+		counter3: 30,
+	});
 
 	return (
 		<>
-			<h1>Counter: {counter}</h1>
+			<h1>Counter1: {counter1}</h1>
+			<h1>Counter2: {counter2}</h1>
+			<h1>Counter3: {counter3}</h1>
 
 			<hr />
 
-			<button onClick={() => setCounter(counter + 1)} className="btn btn-primary">
+			<button
+				onClick={() =>
+					setCounter({
+						counter1: counter1 + 1,
+						counter2,
+						counter3,
+					})
+				}
+				className="btn btn-primary">
 				+1
 			</button>
-			<button onClick={() => setCounter(counter - 1)} className="btn btn-danger">
+			<button
+				onClick={() =>
+					setCounter(({ counter1, counter2, counter3 }) => ({
+						counter1: counter1 - 1,
+						counter2,
+						counter3,
+					}))
+				}
+				className="btn btn-danger">
 				-1
 			</button>
 		</>
