37c1fe8e7c3f687f5d5df9dfdaf1e8734ec27580
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-23 19:41:28 -0700

    Created CounterWithCustomHook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,21 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterWithCustomHook = () => {
+	const { counter, decrement, increment, reset } = useCounter();
+
+	return (
+		<>
+			<h1>Counter With Hook: {counter}</h1>
+			<hr />
+			<button onClick={() => increment(1)} className="btn btn-primary">
+				+1
+			</button>
+			<button onClick={reset} className="btn btn-secondary">
+				Reset
+			</button>
+			<button onClick={() => decrement(1)} className="btn btn-danger">
+				-1
+			</button>
+		</>
+	);
+};
