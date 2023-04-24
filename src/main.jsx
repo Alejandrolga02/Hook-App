1238709973449a96e1fd64d14d7f438184fc11f5
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-24 16:36:26 -0700

    FocusScreen component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -10,7 +11,7 @@ import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { HooksApp } from "./HooksApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<MultipleCustomHooks />
-	</React.StrictMode>
+	// <React.StrictMode>
+	<FocusScreen />
+	// </React.StrictMode>
 );
