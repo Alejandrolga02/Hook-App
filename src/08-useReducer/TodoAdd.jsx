eee194f13a2c609b0d9dfcc5fc9facbe0979b6b8
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-25 10:13:01 -0700

    Added useForm in TodoAdd component

--- src/08-useReducer/TodoAdd.jsx
+++ src/08-useReducer/TodoAdd.jsx
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types'
+import { useForm } from '../hooks/useForm';
 
 export const TodoAdd = ({addTodo}) => {
+	const {onInputChange, description} = useForm({
+		description: ''
+	});
 	
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		
+		if (description.length < 3) return;  
 
-		const description = e.target.querySelector('#description').value;
 		addTodo(description);
 	}
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input id='description' type="text" placeholder="What do you want to do?" className="form-control" />
+			<input name='description' onChange={onInputChange} value={description} type="text" placeholder="What do you want to do?" className="form-control" />
 			<button className="btn btn-primary" type="submit">Add</button>
 		</form>
 	);
