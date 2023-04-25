06f1e796ba0f8913b0fae8899b57b6cb75377336
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-24 19:40:28 -0700

    Memorize and Small component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Memorize } from "./06-memos/Memorize";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
@@ -12,6 +14,6 @@ import { FocusScreen } from "./04-useRef/FocusScreen";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	// <React.StrictMode>
-	<FocusScreen />
+	<Memorize />
 	// </React.StrictMode>
 );
